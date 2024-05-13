e65335986cb5b245ec8e97ff51ceb3846aebde37
laojuaihe22 <jylee073@mymail.sim.edu.sg>
2024-05-13 15:45:45 +0800

    finish changes on admin

--- boundary/SystemAdmin/adminSuspendProfilePage.py
+++ boundary/SystemAdmin/adminSuspendProfilePage.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session, Blueprint, flash
 from controller.SystemAdmin.suspendUserProfileController import SuspendUserProfileController
-from controller.SystemAdmin.viewUserAccountController import ViewUserAccountController
+
 
 suspend_profile_app = Blueprint('suspend_profile_app', __name__)
 
@@ -8,15 +8,17 @@ suspend_profile_app = Blueprint('suspend_profile_app', __name__)
 def suspend_profile_page():
     if request.method == "POST":
         user_email = request.form["email"]
+        user_email_retype = request.form["email_re"]
+        
+        if user_email != user_email_retype:
+            return render_template('adminSuspendAccount.html',message='Email mismatch!')
 
         suspendUserProfile = SuspendUserProfileController()
         is_suspended = suspendUserProfile.suspendUserProfile(user_email)
 
         if is_suspended:
-            flash(f'{user_email}\'s profile suspended!', 'success')
+            flash(f'User Profile successfully suspended!', 'success')
         else:
             flash('Email doesn\'t exists, please enter again', 'error')
     
-    viewUserAccountController = ViewUserAccountController()
-    user_account_data = viewUserAccountController.viewUserAccountData()
-    return render_template('adminSuspendProfile.html', users = user_account_data)
+    return render_template('adminSuspendProfile.html')
