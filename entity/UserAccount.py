1839986e49d2f366fc533f3e52622bb97fdb779b
laojuaihe22 <jylee073@mymail.sim.edu.sg>
2024-05-19 17:15:21 +0800

    testcase changes

--- entity/UserAccount.py
+++ entity/UserAccount.py
@@ -6,8 +6,7 @@ class UserAccount:
     def get_database(self):
         if self.database is None:
             # Establish a connection to the MongoDB server
-            self.database = MongoClient("mongodb+srv://mongo:mongo@cluster0.zj42wez.mongodb.net/?tls=true")
-            # self.database = MongoClient("mongodb://localhost:27017")
+            self.database = MongoClient("mongodb://localhost:27017")
             
         return self.database
     
